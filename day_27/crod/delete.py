561d17da61f6664e75c57da069b8a9d46b0a8f97
maybesayon <sayoncoldplay@gmail.com>
2023-08-29 09:26:17 +0545

    crud complete

--- day_27/crod/delete.py
+++ day_27/crod/delete.py
@@ -1,4 +1,18 @@
+import json
+filename = "students.json"
 
+def delete_student(student_id):
+    with open(filename, 'r') as fp:
+        students = json.loads(fp.read())
+        student = list(filter(lambda x: x["id"] == student_id, students))
+        if student:
+            student = student[0]
+            students.remove(student)
+            with open(filename, 'w') as fw:
+                fw.write(json.dumps(students, indent = 2 ))
+            print("student has been removed")
+        else:
+            print("Invalid student id")
 
-def delete_student():
-    pass
+    repeat = input("Do you want to continue?(y/n) ")
+    return True if repeat.lower() == "y" else False
--- day_27/crod/update.py
+++ day_27/crod/update.py
@@ -1,4 +1,22 @@
+import json
+filename = "students.json"
 
-
-def update_student():
-    pass
+def update_student(student_id):
+    with open(filename, "r+") as fp:
+        students = json.loads(fp.read())
+        student = list(filter(lambda x: x['id'] == student_id, students))
+        if student:
+            student = student[0]
+            key = input("Enter the info you want to update ")
+            if key.lower() not in ["name", "age", "address"]:
+                print("Invalid User Info")
+            else:
+                value = input("Enter the new value ")
+                student[key] = value
+                fp.seek(0)
+                fp.write(json.dumps(students, indent=2))
+                print("Student Updated successfully")
+        else:
+            print("Enter a valid id")
+    repeat = input("Do you want to continue?(y/n) ")
+    return True if repeat.lower() == "y" else False
